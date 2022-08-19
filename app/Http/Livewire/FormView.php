98ad73aa3ef2a6d3472451fb8ea35e10d43ed767
Arun S Kurup <arunskurup81@gmail.com>
2022-08-19 16:27:23 +0530

    livewire add form view

---
+++ app/Http/Livewire/FormView.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class FormView extends Component
+{
+    public function render()
+    {
+        return view('livewire.form-view');
+    }
+}
